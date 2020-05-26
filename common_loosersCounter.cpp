2ea487d63f55c1390126b73e38f0aae9d82a1e9b
Fernando Fabbiano <ffabbiano@fi.uba.ar>
2020-05-26 16:33:00 -0300

    Algunos cambios

--- common_loosersCounter.cpp
+++ common_loosersCounter.cpp
@@ -1,6 +1,6 @@
 #include "common_loosersCounter.h"
 
-LoosersCounter::LoosersCounter() : counter(0) {}
+LoosersCounter::LoosersCounter() : counter(-1) {}
     
 void LoosersCounter::inc() {
     Lock l(m);
