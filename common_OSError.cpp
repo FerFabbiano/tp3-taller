96a0b05e20947d288ae9affd1598fe672b339f81
Fernando Fabbiano <ffabbiano@fi.uba.ar>
2020-05-27 19:27:28 -0300

    Soluciono issue #7

--- common_OSError.cpp
+++ common_OSError.cpp
@@ -1,12 +1,9 @@
-#include <errno.h>
 #include <iostream>
 #include <string>
 
 #include "common_OSError.h"
 
-OSError::OSError(const std::string &error) noexcept {
-    this->mssg_error = error;
-}
+OSError::OSError(const std::string &error) noexcept : mssg_error(error){}
 
 const char* OSError::what() const noexcept{
     return this->mssg_error.c_str();
